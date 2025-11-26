8800050287d8120a9d14364f159aaea84ed7c571
Pierangelo <pierangelocaporaso1994@gmail.com>
2025-11-26 11:09:04 +0100

    Protein Folding

--- src/main/java/com/piera/ProteinFoldingAction.java
+++ src/main/java/com/piera/ProteinFoldingAction.java
@@ -2,15 +2,14 @@ package com.piera;
 
 import it.uniroma1.di.tmancini.teaching.ai.search.Action;
 
-public class ProteinFoldingAction extends Action{
+public class ProteinFoldingAction extends Action {
 
     ProteinFoldingState state;
     int x;
     int y;
     char c;
 
-
-    public ProteinFoldingAction(ProteinFoldingState state, int x, int y, char c){
+    public ProteinFoldingAction(ProteinFoldingState state, int x, int y, char c) {
         // l'azione che aggiunge c (con c in {"H", "P"}) in posizione (x,y)
         this.state = state;
         this.x = x;
@@ -27,75 +26,80 @@ public class ProteinFoldingAction extends Action{
         }
 
         int newContacts = countNewContacts();
-        return (this.state.getPsize()/2) + (-1.0 * newContacts);
+        return (this.state.getPsize() / 2) + (-1.0 * newContacts);
     }
 
-    // Counts the number of new H-H contacts formed by placing the aminoacid at (x, y)
+    // Counts the number of new H-H contacts formed by placing the aminoacid at (x,
+    // y)
     public int countNewContacts() {
         int newContacts = 0;
-        
+
         char[][] grid = this.state.getGrid();
         int size = grid.length;
 
-        int realX = (this.state.getPsize()-1) + x;
-        int realY = (this.state.getPsize()-1) + y;
+        int realX = (this.state.getPsize() - 1) + x;
+        int realY = (this.state.getPsize() - 1) + y;
 
         int prevX = this.state.getX();
         int prevY = this.state.getY();
 
         // check left
-        if (realX - 1 >= 0){
-            if (grid[realX - 1][realY] == 'H' && !(prevX == x -1 && prevY == y)){
+        if (realX - 1 >= 0) {
+            if (grid[realX - 1][realY] == 'H' && !(prevX == x - 1 && prevY == y)) {
                 newContacts += 2;
             }
         }
 
         // check right
-        if (realX + 1 < size){
-            if (grid[realX + 1][realY] == 'H' && !(prevX == x +1 && prevY == y)){
+        if (realX + 1 < size) {
+            if (grid[realX + 1][realY] == 'H' && !(prevX == x + 1 && prevY == y)) {
                 newContacts += 2;
             }
         }
 
         // check up
-        if (realY - 1 >= 0){
-            if (grid[realX][realY - 1] == 'H' && !(prevX == x && prevY == y -1)){
+        if (realY - 1 >= 0) {
+            if (grid[realX][realY - 1] == 'H' && !(prevX == x && prevY == y - 1)) {
                 newContacts += 2;
             }
         }
 
         // check down
-        if (realY + 1 < size){
-            if (grid[realX][realY + 1] == 'H' && !(prevX == x && prevY == y +1)){
+        if (realY + 1 < size) {
+            if (grid[realX][realY + 1] == 'H' && !(prevX == x && prevY == y + 1)) {
                 newContacts += 2;
             }
         }
 
-        if (grid[realX][realY + 1] == 'F'){
+        if (grid[realX][realY + 1] == 'F') {
             newContacts += 1;
         }
 
         return newContacts;
     }
 
+    /*
+     * =========================
+     * 
+     * Add equals(Object oo), hashCode(), clone() and toString()!
+     * 
+     * =========================
+     */
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+        if (!o.getClass().equals(this.getClass()))
+            return false;
+        ProteinFoldingAction oo = (ProteinFoldingAction) o;
+        return this.state == oo.state && this.x == oo.x && this.y == oo.y && this.c == oo.c;
+    }
 
-    /*=========================
+    public int hashCode() {
+        return state.hashCode() + x * 7 + y * 11 + c * 17;
+    }
 
-    Add equals(Object oo), hashCode(), clone() and toString()!
+    public String toString() {
+        return "Add " + c + " at (" + x + "," + y + ")";
+    }
 
-     =========================*/
-     public boolean equals(Object o) {
-		if (o == null) return false;
-		if (!o.getClass().equals(this.getClass())) return false;
-		ProteinFoldingAction oo = (ProteinFoldingAction)o;
-		return this.state == oo.state && this.x == oo.x && this.y == oo.y && this.c == oo.c;
-	}
-	public int hashCode() {
-		return state.hashCode() + x*7 + y*11 + c*17;
-	}
-
-	public String toString() {
-		return "Add " + c + " at (" + x + "," + y + ")";
-	}
-    
 }
